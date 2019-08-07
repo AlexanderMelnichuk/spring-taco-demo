70e82acb33036b61b5beb86a2bbf25efe87c731a
Alexander Melnichuk <alexander.melnichuk@gmail.com>
2019-08-08 04:09:56 +1000

    Added controller for env vars display

---
+++ src/main/java/tacos/EnvironmentController.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EnvironmentController {
+
+    @RequestMapping(path = "/env")
+    public String test() {
+        return String.format("Classpath:<br>%s", System.getProperty("java.class.path"));
+    }
+}
